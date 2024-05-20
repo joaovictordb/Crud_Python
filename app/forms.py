3337260a1f59b8a5a1a92251aa945801ac936a68
Joao victor <joaovictoraloof@gmail.com>
2024-05-19 22:46:13 -0300

    primeira versão

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from app.models import Pessoa
+
+# Create the form class.
+class PessoaForm(ModelForm):
+     class Meta:
+        model = Pessoa
+        fields = ['nome', 'email', 'idade']
